d229ef5b6d7d35ada7c707f4ca6990cbf2339cf5
Alisia Reveli <alisia.reveli@bizaway.com>
2025-08-31 21:34:42 +0200

    docker

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,15 +4,14 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 
 const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
-  transport: isDevelopment ? {
+  transport:  {
     target: 'pino-pretty',
     options: {
       colorize: true,
       translateTime: 'SYS:standard',
       ignore: 'pid,hostname'
     }
-  } : undefined
-})
+  } })
 
 export const log = {
   info: (message: string, data?: any) => logger.info(data, message),
